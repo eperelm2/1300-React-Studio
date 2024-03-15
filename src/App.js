95fcdeed6dfaa50ef60a1c30cc31801171792927
eperelm2 <emily_perelman@brown.edu>
2024-03-15 14:25:05 -0400

    almost ready to deploy

--- src/App.js
+++ src/App.js
@@ -12,17 +12,79 @@ function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
+  const [cart, setCart] = useState([])
+  const [itemQuantity, setItemQuantity] = useState({});
+  const [cartPrice, setPrice] = useState(0)
+
+
+  const addToCart = (itemName, itemPrice) => {
+    if (cart.includes(itemName)) {
+      const updatedItemQuantity = { ...itemQuantity };
+      updatedItemQuantity[itemName] += 1;
+      setItemQuantity(updatedItemQuantity);
+    } else {
+      const updatedCart = [...cart, itemName];
+      setCart(updatedCart);
+  
+      const updatedItemQuantity = { ...itemQuantity };
+      updatedItemQuantity[itemName] = 1;
+      setItemQuantity(updatedItemQuantity);
+
+   
+    }
+    const updatedCartPrice = + (cartPrice + itemPrice).toFixed(2)
+    setPrice(updatedCartPrice)
+  };
+
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+    <div class="bakery-side">
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+            <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
+            <div class="bakery-grid">
+            
+            
+            {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+            
+            <div class="bakery-item">
+                <img src={item.image} alt="Bakery-img" />
+                <h3 class="bakery-item-desc"> {item.name} </h3>
+                <p class="bakery-item-desc"> {item.description}</p>
+                <div className="price-and-button">
+                      <p className="price">${item.price}</p>
+                      <button className="add-to-cart" onClick={() => addToCart(item.name, item.price)}>
+                        Add to cart
+                      </button>
+              </div>
+            </div>
+
+            // <p> {item.name}, {item.description}, </p> // replace with BakeryItem component //Bakery Item {index},
+            ))}
+        </div>
+    
+    </div>
+     
       <div>
-        <h2>Cart</h2>
+        <h2 class="cart">My Cart</h2>
         {/* TODO: render a list of items in the cart */}
+        {cart.length === 0 ? (
+          <p class="cart">There is nothing in your cart right now!</p>
+        ) : (
+          <div className="cart">
+            {cart.map((item, index) => (
+              <div>
+                {itemQuantity[item]}x, {item}
+              </div>
+            ))}
+
+            <div class="total"> 
+            Total: ${cartPrice}
+            </div>
+
+            
+          </div>
+        )}
       </div>
     </div>
   );
